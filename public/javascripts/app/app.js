06baa90e221c7ff9de7df5ad9b2bcef5d3d74761
Sumeet Singh <ortuna@gmail.com>
2013-02-16 20:25:00 -0500

    Refactored with right way to do views in emberjs

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -17,37 +17,52 @@ Nomnom.catView = Ember.View.extend({
   tagName: 'img',
   classNames: ['movable', 'large'],
   attributeBindings: ['src'],
-  src: 'http://placehold.it/150x150'
-});
-
-var classes = ['large', 'small', 'medium'];
-var images = [];
-
-for(var i = 0 ; i < 10; i++) {
-  var index =  Math.round(Math.random()*3);
-  view = Nomnom.catView.create();
-  view.classNames = [classes[index]];
-  view.append();
-  images.push(view);
-}
-
-Nomnom.setupPositions = function() {
-  var docWidth  = $(document).width();
-  var docHeight = $(document).height();
-  for(var i = 0; i < images.length; i ++) {
-    var $el = $("#" + images[i].get('elementId'));
+  src: 'http://placehold.it/150x150',
+  didInsertElement: function(){
+    this.setupPosition();
+  },
+  setupPosition: function() {
+    var docWidth  = $(document).width();
+    var docHeight = $(document).height();
+    var $el = $("#" + this.get('elementId'));
     var left = Math.round(Math.random() * docWidth  - $el.width());
     var top  = Math.round(Math.random() * docHeight - $el.height());
+
     $el.css("left", left);
     $el.css("top", top);
 
-    $el.transition({ y: - (top + docHeight), duration: 5000 });
+    var duration = 1000;
+    if($el.hasClass('large'))
+      duration = duration * 10;
+    else if($el.hasClass('medium'))
+      duration = duration * 9.5;
+    else
+      duration = duration * 9;
+
+    $el.transition({ y: - (top + docHeight), duration: duration });
   }
-
-}
-
-$(document).ready(function(){
-  var moveInterval = setTimeout(Nomnom.setupPositions, 500);
-  var moveTimeout  = setTimeout(Nomnom.moveImages, 1000);
 });
 
+Nomnom.catContainer = Ember.View.extend({
+  types: ['large', 'small', 'medium'],
+  templateName: 'cats',
+  init: function(){
+    this.populateImages();
+    return this._super();
+  },
+  populateImages: function(){
+    for(var i = 0 ; i < 10; i++) {
+      var index =  Math.round(Math.random()*2);
+      view = Nomnom.catView.create();
+      view.classNames = [this.types[index]];
+      view.append();
+      this.get('childViews').pushObject(view);
+    }
+  }
+});
+
+
+
+$(document).ready(function(){
+  var cats = Nomnom.catContainer.create();
+});
